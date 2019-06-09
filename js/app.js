532234bff1cada500caccdffb4c00623ae73d55f
lokers778 <lokers778@gmail.com>
2019-06-09 21:27:42 +0200

    change to duck

--- js/app.js
+++ js/app.js
@@ -1,82 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function Furry() {
+    function Duck() {
         this.x = 0;
         this.y = 0;
         this.direction = "";
 
     }
 
-    function Coin() {
+    function Bread() {
         this.x = Math.floor(Math.random() * 10);
         this.y = Math.floor(Math.random() * 10);
     }
 
     function Game() {
         this.board = document.querySelectorAll("#board div");
-        this.furry = new Furry();
-        this.coin = new Coin();
+        this.duck = new Duck();
+        this.bread = new Bread();
         this.score = 0;
         this.index = function (x, y) {
             return x + (y * 10)
         };
-        this.showFurry = function () {
+        this.showDuck = function () {
 
-            this.board[this.index(this.furry.x, this.furry.y)].classList.add("furry");
+            this.board[this.index(this.duck.x, this.duck.y)].classList.add("duck");
         }
-        this.showCoin = function () {
-            this.board[this.index(this.coin.x, this.coin.y)].classList.add("coin");
+        this.showBread = function () {
+            this.board[this.index(this.bread.x, this.bread.y)].classList.add("bread");
         }
-        this.moveFurry = function () {
-            if (this.furry.direction === "right") {
-                this.furry.x = this.furry.x + 1;
-            } else if (this.furry.direction === "left") {
-                this.furry.x = this.furry.x - 1;
-            } else if (this.furry.direction === "up") {
-                this.furry.y = this.furry.y + 1;
-            } else if (this.furry.direction == "down") {
-                this.furry.y = this.furry.y - 1;
+        this.moveDuck = function () {
+            if (this.duck.direction === "right") {
+                this.duck.x = this.duck.x + 1;
+            } else if (this.duck.direction === "left") {
+                this.duck.x = this.duck.x - 1;
+            } else if (this.duck.direction === "up") {
+                this.duck.y = this.duck.y + 1;
+            } else if (this.duck.direction == "down") {
+                this.duck.y = this.duck.y - 1;
             }
             this.wallColision();
-            this.checkCoinCollision();
-            this.hideVisibleFurry();
-            this.showFurry();
+            this.checkBreadCollision();
+            this.hideVisibleDuck();
+            this.showDuck();
         }
 
         var self = this;
         this.startGame = setInterval(function () {
-            self.moveFurry()
+            self.moveDuck()
         }, 250)
 
-        this.hideVisibleFurry = function () {
-            var target = document.querySelector(".furry");
-            target.classList.remove("furry");
+        this.hideVisibleDuck = function () {
+            var target = document.querySelector(".duck");
+            target.classList.remove("duck");
         }
-        this.movingFurry = function (event) {
+        this.movingDuck = function (event) {
             switch (event.which) {
                 case 37:
-                    this.furry.direction = "left";
+                    this.duck.direction = "left";
                     break;
                 case 39:
-                    this.furry.direction = "right";
+                    this.duck.direction = "right";
                     break;
                 case 40:
-                    this.furry.direction = "up";
+                    this.duck.direction = "up";
                     break;
                 case 38:
-                    this.furry.direction = "down";
+                    this.duck.direction = "down";
                     break;
             }
 
 
         };
         this.score = 0;
-        this.checkCoinCollision = function () {
-            if (this.coin.x === this.furry.x && this.coin.y === this.furry.y) {
+        this.checkBreadCollision = function () {
+            if (this.bread.x === this.duck.x && this.bread.y === this.duck.y) {
                 console.log("point");
-                var targetCoin = document.querySelector(".coin");
-                targetCoin.classList.remove("coin");
-                this.coin = new Coin();
-                this.showCoin();
+                var targetBread = document.querySelector(".bread");
+                targetBread.classList.remove("bread");
+                this.bread = new Bread();
+                this.showBread();
                 var Score = document.querySelector("#score > div > strong")
                 this.score++;
                 Score.innerText = this.score;
@@ -86,9 +86,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
         this.wallColision = function () {
-            if (this.furry.x < 0 || this.furry.x > 9 || this.furry.y < 0 || this.furry.y > 9){
+            if (this.duck.x < 0 || this.duck.x > 9 || this.duck.y < 0 || this.duck.y > 9){
                 clearInterval(this.startGame);
-                this.hideVisibleFurry();
+                this.hideVisibleDuck();
                 alert("GAME OVER " + "YOUR SCORE "+ this.score)
             }
         }
@@ -96,11 +96,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     document.addEventListener("keydown", function (event) {
-        game1.movingFurry(event)
+        game1.movingDuck(event)
     })
 
     var game1 = new Game();
-    game1.showFurry();
-    game1.showCoin();
+    game1.showDuck();
+    game1.showBread();
     game1.startGame;
 });
